ad923f43eb119ed0050a794ebdc4eb11b22bb5fe
Leonel Angel <leonelgo66@gmail.com>
2024-06-17 15:44:53 -0300

    first commit

---
+++ Text.js
@@ -0,0 +1,50 @@
+
+// export const txt = `
+//
+// `;
+
+export const txt = `
+Conversación 1: Reunión de Equipo
+Contexto: Una reunión semanal de un equipo de desarrollo de software.
+
+Personas:
+
+Sarah (Gerente de Proyecto)
+Alex (Desarrollador)
+Emily (Diseñadora)
+Mark (Analista)
+Sarah: Good morning, everyone. Let's start with a quick update on our progress. Alex, how's the implementation of the new feature going?
+
+Alex: Morning, Sarah. I've almost finished coding it. Just need to run some tests today.
+
+Emily: I've finalized the UI design for the feature. Alex, I'll send you the mockups by noon.
+
+Mark: I've been reviewing the user stories and refining some acceptance criteria. Sarah, I think we're on track for the sprint review next week.
+
+Sarah: Great to hear, Mark. Let's aim to wrap up testing by Thursday and plan for a demo on Friday. Anything else we should discuss?
+
+Emily: Just a heads-up, Sarah. We might need some UX feedback before finalizing the designs.
+
+Sarah: Noted, Emily. Let's schedule a quick review session tomorrow. Anything else? Alright, let's keep up the momentum. Thanks, everyone.
+
+Conversación 2: Discusión sobre Proyecto
+Contexto: Dos colegas discuten un proyecto de desarrollo de software en el almuerzo.
+
+Personas:
+
+Tom (Desarrollador)
+Lisa (QA Tester)
+Tom: Hey, Lisa. How's testing going for the new release?
+
+Lisa: Hey, Tom. It's going okay, but I've found a few bugs in the payment module. I'll need your help to reproduce them.
+
+Tom: Sure thing. I'll check my code and see if I can pinpoint the issue. Have you documented the steps to reproduce?
+
+Lisa: Yes, I have. I'll send them over to you after lunch.
+
+Tom: Thanks, Lisa. By the way, have you heard anything about the deployment schedule?
+
+Lisa: Not yet, but I'll check with Sarah after this. We might need to push back if these bugs aren't resolved soon.
+
+Tom: Understood. Let's sync up with Sarah and keep each other posted.
+`;
---
+++ script_ls.js
@@ -0,0 +1,229 @@
+import { txt } from './Text.js';
+
+const WORDS = 'WORDS';
+
+const word_items_init = {
+  A: [],
+  B: [],
+  C: [],
+  D: [],
+  E: [],
+  F: [],
+  G: [],
+  H: [],
+  I: [],
+  J: [],
+  K: [],
+  L: [],
+  M: [],
+  N: [],
+  O: [],
+  P: [],
+  Q: [],
+  R: [],
+  S: [],
+  T: [],
+  U: [],
+  V: [],
+  W: [],
+  X: [],
+  Y: [],
+  Z: []
+};
+
+function loadInitialData() {
+  const storedData = localStorage.getItem(WORDS);
+  return storedData ? JSON.parse(storedData) : word_items_init;
+}
+
+function saveItem(data) {
+  localStorage.setItem(WORDS, JSON.stringify(data));
+}
+
+const wordsItems = loadInitialData();
+
+// Insert text
+const $insertText = document.getElementById('insertText');
+$insertText.innerText = txt;
+
+const $wordInput = document.getElementById('wordInput');
+const $translationInput = document.getElementById('translationInput');
+const $addButton = document.getElementById('addButton');
+
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+function superCapitalize(cadena) {
+  let palabras = cadena.split(/[,\s]+/);
+
+  let resultado = palabras.map((palabra) => {
+    return palabra.charAt(0).toUpperCase() + palabra.slice(1);
+  });
+
+  return resultado.join(', ');
+}
+
+// Add word - translation
+$addButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  const word = $wordInput.value.trim();
+  const translation = $translationInput.value.trim();
+
+  if (word && translation) {
+    const newWord = {
+      id: Date.now(),
+      word: capitalize(word),
+      translation: superCapitalize(translation)
+    };
+    const firstLetter = word.charAt(0).toUpperCase();
+
+    try {
+      wordsItems[firstLetter].push(newWord);
+      saveItem(wordsItems);
+
+      $wordInput.value = '';
+      $translationInput.value = '';
+    } catch (error) {
+      console.error('Error al agregar palabra:', error);
+    }
+  } else {
+    alert('Por favor, ingresa una palabra y su traducción.');
+  }
+});
+
+const $alphabet = document.querySelector('.alphabet');
+const $wordList = document.getElementById('wordList');
+const $modal = document.querySelector('.modal');
+const $closeButton = document.querySelector('.close-button');
+
+$alphabet.addEventListener('click', async (e) => {
+  openModal();
+  const letterValue = e.target.value;
+
+  const data = wordsItems[letterValue];
+
+  if (!data || data.length === 0) {
+    $wordList.style.textAlign = 'center';
+    $wordList.innerText = 'No hay palabras agregadas';
+    return;
+  }
+
+  renderWordList(data);
+});
+
+// Función para renderizar la lista de palabras en la modal
+const renderWordList = (data) => {
+  $wordList.innerHTML = '';
+  const fragment = document.createDocumentFragment();
+
+  data.forEach(({ id, word, translation }) => {
+    const div = document.createElement('div');
+    div.classList.add('form-row');
+    const formRowHTML = `
+      <form id="${id}">
+        <input type="text" name="word" placeholder="Palabra" class="word-input" value='${word}' >
+        <input type="text" name="translation" placeholder="Traducción" class="translation-input" value="${translation}">
+        <button type="submit" class="edit-button">Editar</button>
+        <button type="submit" class="delete-button">Eliminar</button>
+      </form>
+    `;
+    div.innerHTML = formRowHTML;
+    fragment.appendChild(div);
+  });
+
+  $wordList.appendChild(fragment);
+};
+
+// Evento para manejar la edición y eliminación de palabras
+$wordList.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const form = e.target;
+  const word = form.word.value.trim();
+  const translation = form.translation.value.trim();
+  const id = form.id;
+
+  if (word && translation) {
+    const newWord = {
+      word,
+      translation
+    };
+    const firstLetter = word.charAt(0).toUpperCase();
+
+    try {
+      const index = wordsItems[firstLetter].findIndex((item) => item.id == id);
+
+      if (e.submitter.classList.contains('edit-button')) {
+        if (index !== -1) {
+          wordsItems[firstLetter][index] = {
+            ...wordsItems[firstLetter][index],
+            ...newWord
+          };
+          saveItem(wordsItems);
+        }
+      }
+
+      if (e.submitter.classList.contains('delete-button')) {
+        const confirmDelete = confirm(`¿Eliminar la palabra ${word}?`);
+        if (confirmDelete) {
+          wordsItems[firstLetter].splice(index, 1);
+          saveItem(wordsItems);
+        }
+      }
+
+      // Actualizar la lista después de editar o eliminar
+      renderWordList(wordsItems[firstLetter]);
+    } catch (error) {
+      console.error('Error al editar o eliminar la palabra:', error);
+    }
+  }
+});
+
+// Función para abrir la modal
+const openModal = () => {
+  $modal.style.display = 'flex';
+};
+
+// Función para cerrar la modal
+const closeModal = () => {
+  $modal.style.display = 'none';
+  $wordList.innerHTML = ''; // Limpiamos la lista al cerrar la modal
+};
+
+// Evento para cerrar la modal al hacer clic en el botón de cerrar
+$closeButton.addEventListener('click', closeModal);
+
+const $saveInDBBtn = document.getElementById('saveDBButton');
+$saveInDBBtn.addEventListener('click', () => {
+  saveWordsInDB(wordsItems);
+});
+
+// Evento para cerrar la modal al hacer clic afuera del modal-content
+$modal.addEventListener('click', (e) => {
+  console.log();
+  if (e.target.classList.contains('modal')) {
+    closeModal();
+  }
+});
+
+document.getElementById('saveDBButton').addEventListener('click', () => {
+  const content = JSON.stringify(wordsItems);
+  const blob = new Blob([content], { type: 'text/plain' });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'words_en_es.txt';
+  a.click();
+  URL.revokeObjectURL(url);
+});
+
+setInterval(() => {
+  const content = JSON.stringify(wordsItems);
+  const blob = new Blob([content], { type: 'text/plain' });
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  a.href = url;
+  a.download = 'words_en_es.txt';
+  a.click();
+  URL.revokeObjectURL(url);
+}, 600000);
